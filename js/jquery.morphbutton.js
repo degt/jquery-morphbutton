608dc61cc5e9992350059aac894f7e84668ba877
Daniel Gutierrez <daniel.gutierrez@degt.cl>
2014-09-17 17:27:23 -0300

    Demo added

--- js/jquery.morphbutton.js
+++ js/jquery.morphbutton.js
@@ -12,41 +12,40 @@
               var $buttonClose = $this.find('button.morphbutton-close');
               var $content = $this.find('.morphbutton-content');
 
+              //Setup position
               $content.css({
                 top: $this.offset().top,
                 left: $this.offset().left,
-                width: $this.css('width'),
-                height: $this.css('height')
+                width: $button.css('width'),
+                height: $button.css('height')
               });
 
+              $content.addClass('no-transition');
+
               $button.click(function(){
-                
-                
-                // $content.css('visibility', 'visible');
-                // $content.css({
-                //   top: '',
-                //   left: '',
-                //   width: $this.css('width'),
-                //   height: $this.css('height'),
-                //   visibility: 'hidden'
-                // });
-                // $content.css({
-                //   top: $this.offset().top,
-                //   left: $this.offset().left
-                // //   width: $button.css('width'),
-                // //   height: $button.css('height'),
-                // //   display: 'block'
-                // });
+          
+                //Avoid weird moves
+                $content.addClass('no-transition');
 
-                $content.css('opacity', 1);
+                //Reset position
+                $content.css({
+                  top: $this.offset().top,
+                  left: $this.offset().left,
+                  width: $button.css('width'),
+                  height: $button.css('height'),
+                  opacity: 1
+                });
 
+                //Start animating
                 $content.delay(300).queue(function(){
+                  $content.removeClass('no-transition');
                   $this.addClass('active');
                   $(this).dequeue();
                 }).delay(500).queue(function(){
                   $this.addClass('open');
                   $(this).dequeue();
                 });
+
                 
               });
 
@@ -54,6 +53,14 @@
                 $this.removeClass('active');
                 $this.removeClass('open');
 
+                //Reset position
+                $content.css({
+                  top: $this.offset().top,
+                  left: $this.offset().left,
+                  width: $button.css('width'),
+                  height: $button.css('height')
+                });
+                
                 $content.delay(500).queue(function(){
                   $content.css('opacity', 0);
                   $(this).dequeue();
@@ -61,67 +68,6 @@
 
               });
 
-              // 
-              // 
-
-              // //Init
-              // var minW = $button.css('width');
-              // var maxW = $content.css('width');
-              // var minH = $button.css('height');
-              // var maxH = $content.css('height');
-
-              // var posY = $this.offset().top;
-              // var posX = $this.offset().left;
-
-
-              // // $this.data('width-start', $button.css('width'));
-              // // $this.data('width-finish', $content.css(
-              // // $this.data('height-start', $button.css('height'));
-              // // $this.data('height-finish', $content.css('height'));
-
-              // //Set min height/width
-              // $this.width(minW);              
-              // $this.height(minH);
-
-              // console.log(minH);
-
-              // $button.click(function(){
-              //   $button.hide();
-              //   // $this.width(maxW);
-              //   // $this.height(maxH);
-
-              //   console.log(maxH);
-
-              //   $this.css({
-              //     width: maxW,
-              //     height: maxH,
-              //     top: 100,
-              //     left: ($(window).width() / 2) - (parseInt(maxH) / 2)
-              //   });
-              // });
-
-              // $buttonClose.click(function(){
-              //   $button.show();
-                
-              //   $this.css({
-              //     width: minW,
-              //     height: minH,
-              //     top: posY,
-              //     left: posX
-              //   });
-              // });
-
-              // $buttonClose.click();
-
-
-
-
-
-
-              //console.log($this.data());
-
-
-              //$this.data('width-start', $this.width());
           });
       }
   });
@@ -133,21 +79,3 @@
   
 }(jQuery));
 
-// (function($){
-//   $.fn.extend({
-//       morphButton: function(options) {
-//           this.defaultOptions = {};
-//           var settings = $.extend({}, this.defaultOptions, options);
-
-//           return this.each(function() {
-//               var $this = $(this);
-//           });
-//       }
-//   });
-
-//   //Auto Init
-//   $(document).ready(function(){
-
-//     $(".morphButton").morphButton();
-//   });
-// })(jQuery);
