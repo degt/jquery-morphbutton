13870e006923ae8b5f54e2175c24707e26de0399
Daniel Gutiérrez <daniel.gutierrez@degt.cl>
2015-06-02 15:17:11 -0300

    unecessary comments removed

--- js/jquery.morphbutton.js
+++ js/jquery.morphbutton.js
@@ -7,77 +7,7 @@
           var settings = $.extend({}, this.defaultOptions, options);
           
           return this.each(function() {
-
               new MorphButton(this, options);
-
-          
-              // var $this = $(this);
-              // var $button = $this.find('button.morphbutton-open');
-              // var $buttonClose = $this.find('button.morphbutton-close');
-              // var $content = $this.find('.morphbutton-content');
-
-
-              // //Setup position
-              // $content.css({
-              //   top: $this.offset().top,
-              //   left: $this.offset().left,
-              //   width: $button.css('width')
-              //   // height: $button.css('height')
-              // });
-
-              // $content.addClass('no-transition');
-
-              // $button.click(function(){
-
-              //   //Avoid weird moves
-              //   $content.addClass('no-transition');
-              //   $this.addClass('animating');
-
-              //   //Reset position
-              //   $content.css({
-              //     top: $button.offset().top - $(window).scrollTop(),
-              //     left: $button.offset().left,
-              //     width: $button.css('width'),
-              //     maxHeight: $button.css('height'),
-              //     opacity: 1
-              //   });
-
-              //   //Start animating
-              //   $content.delay(100).queue(function(){
-              //     $content.removeClass('no-transition');
-              //     $this.addClass('active');
-              //     $content.height('');
-              //     $(this).dequeue();
-              //   }).delay(500).queue(function(){
-              //     $this.addClass('open');
-              //     $this.removeClass('animating');
-              //     $(this).dequeue();
-              //   });
-
-                
-              // });
-
-              // $buttonClose.click(function(){
-              //   $this.removeClass('active');
-              //   $this.removeClass('open');
-              //   $this.addClass('animating');
-
-              //   //Reset position
-              //   $content.css({
-              //     top: $button.offset().top - $(window).scrollTop(),
-              //     left: $button.offset().left,
-              //     width: $button.css('width'),
-              //     maxHeight: $button.css('height')
-              //   });
-
-              //   $content.delay(500).queue(function(){
-              //     $content.css('opacity', 0);
-              //     $this.removeClass('animating');
-              //     $(this).dequeue();
-              //   });
-
-              // });
-
           });
       }
 
@@ -104,13 +34,11 @@
           morphbutton.buttonClose = morphbutton.target.find('.morphbutton-close');
 
       morphbutton.button.click(function(evt){
-        //morphbutton.action();
         morphbutton.open();
         return false;
       });
 
       morphbutton.buttonClose.click(function(evt){
-        //morphbutton.action();
         morphbutton.close();
         return false;
       });
@@ -144,9 +72,7 @@
         }
 
         $(this).dequeue();
-      });                
-
-      //   evt.preventDefault();
+      });
 
     },
     open:function(){
@@ -208,8 +134,6 @@
         $('body').css('overflow', '');
         $(this).dequeue();
       }); 
-
-
     }
 
   }
